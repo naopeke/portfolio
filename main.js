ebe74f14eb84dc414660e544fcaf1ba27f36d870
naopeke <naopeke@protonmail.com>
2024-04-15 19:33:45 +0200

    nav js in progress

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//show menu
+//*show menu
 
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
@@ -7,6 +7,7 @@ const navMenu = document.getElementById('nav-menu'),
 // if navToggle exists, add 'show-menu' class to navMenu
 if(navToggle){
     navToggle.addEventListener('click', ()=> {
+        console.log('clicked showmenu');
         navMenu.classList.add('show-menu');
     });
 }
@@ -14,6 +15,24 @@ if(navToggle){
 // if navClose exists, remove 'show-menu' class from navMenu
 if(navClose){
     navClose.addEventListener('click', ()=>{
+        console.log('clicked hidemenu');
         navMenu.classList.remove('show-menu');
     });
+}
+
+
+//* Remove menu for mobile
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(navlink => navlink.addEventListener('click', linkAction));
+
+
+//*change background header
+const scrollHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('scroll-header'):header.classList.remove('scroll-header');
 }
