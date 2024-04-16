5af2b8cbfbf68a44a10ef5ee70694de5327657bd
naopeke <naopeke@protonmail.com>
2024-04-16 18:26:08 +0200

    contact in progress

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-//*show menu
-
+/**
+ * Toggles the visibility of the navigation menu when the toggle button is clicked.
+ */
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close');
@@ -20,8 +21,10 @@ if(navClose){
     });
 }
 
-
-//* Remove menu for mobile
+/**
+ *  Removes the 'show-menu' class from the navigation menu when any navigation link is clicked.
+ * This function is intended for mobile views where the menu should hide after a selection is made.
+ */
 const navLink = document.querySelectorAll('.nav__link');
 
 const linkAction = () => {
@@ -30,9 +33,12 @@ const linkAction = () => {
 }
 navLink.forEach(item => item.addEventListener('click', linkAction));
 
-
-//*change background header
-const scrollHeader = () => {
+/**
+ * Changes the background of the header based on the scroll position of the window.
+ * Adds 'shadow-header' class to header if window is scrolled more than 50 viewport height.
+ */
+const shadowHeader = () => {
     const header = document.getElementById('header');
-    this.scrollY >= 50 ? header.classList.add('scroll-header'):header.classList.remove('scroll-header');
+    this.scrollY >= 50 ? header.classList.add('shadow-header'):header.classList.remove('shadow-header');
 }
+window.addEventListener('scroll', shadowHeader);
